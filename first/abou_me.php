eb8a1c3448243f07ade0402a3f6e79e0946b805b
Mawebsite1 <91473487+Mawebsite1@users.noreply.github.com>
2021-10-01 12:15:44 +0200

    Add files via upload

--- first/abou_me.php
+++ first/abou_me.php
@@ -4,7 +4,13 @@
     <meta charset="utf-8">
     <title>about me</title>
     <link rel="icon" type="image/png" href="https://i.pinimg.com/564x/aa/69/87/aa69876bcaba689b7a7fd96383c7be24.jpg" sizes="16x16">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
+
+    <style>
+        .imig{
+            float: right;
+        }
+    </style>
 
 
 </head>
@@ -44,13 +50,46 @@
                 <img src="https://www.facebook.com/zmienazweuzytkownikaa/" class="custom-logo sp-no-webp" alt="HTML.com logo" height="53" width="250" srcset="https://html.com/wp-content/uploads/html-com-logo.png">
             </picture>
         </a>
-    </div></center>
+    </div></center><br/>
 
 <div>
-I like drawing, i don't draw a lot but i love collecting ideas which u can find <a href="https://pl.pinterest.com/xHeliumx/_saved/">here</a>.
+<p>
+    <img class="imig" src="https://i.pinimg.com/564x/87/2f/57/872f57c9b93dd725a81412b692750df2.jpg" style="width:170px;height:170px;margin-left:15px;">
+    <h3>I like drawing. I don't draw a lot but i love collecting ideas which u can find <a href="https://pl.pinterest.com/xHeliumx/_saved/">here</a> on my Pinterest. Interesting thing about
+    drawing is that Evidence has shown that doodling does actually improve memory. It's not just absent-minded, abstract doodling that helps the brain either; drawing concepts and physical
+        objects forces your brain to engage with a subject in new and different ways, enhancing your understanding.</h3>
+
+</p>
 </div>
 
+<div>
+    <center><h2>For those who want to draw better, here are a few recommendations:</h2></center>
+    <h4>1. Go draw something. Repeat.</h4>
+    Practice leads to improvement. You won’t get any better unless you engage in the attempt. The more you draw the more confident you’ll become.
 
+    <h4>2. Look at drawings.</h4>
+    Whether simple line drawings or meticulously detailed renderings, you can learn a lot from looking at the work of others. How did they use line and shape? How did they shade?
+
+    <h4>3. Draw from drawings.</h4>
+    This may sound peculiar, but what can you learn by copying a Da Vinci or Michaelangelo sketch? Tons. Learn from the masters by copying them. Really. They won’t mind.
+
+    <h4>4. Draw from photographs.</h4>
+    For many, it’s easier to reproduce an image that’s already two-dimensional than to reproduce an actual object, person or environment. When you’re working from photos, look at edges, shapes and angles. Don’t trace. Draw. Keep in mind that many photographs include distorted shapes, scale, and proportions. Use photos as reference, but pursue accurate proportions.
+
+    <h4>5. Draw from life.</h4>
+    If you’re just starting out, pick simple objects and work your way up to complex ones. Go ahead and try your and at drawing people and your pets. Draw your furniture and your living spaces. Do you enjoy coffee? Draw your coffee cup. Here’s a challenge: draw your hand. Hands and feet are the most complex parts of your anatomy and are readily available subject matter. If you can master these, you’ll pretty much be able to draw anything.
+
+    <h4>6. Take a class.</h4>
+    A class will keep you accountable. A teacher will correct your weaknesses. Watching others draw is immensely beneficial for building your own observational skills. Where do you find a class? Check your local university extension, community adult school, YMCA or community college. Another source is your local art supply store, where artists post notices of drawing meet-ups, uninstructed sessions with models, or private instruction.
+
+    <h4>7. Keep a sketchbook.</h4>
+    Meaning, keep it with you, open it up and work at filling it. It will serve as a reminder to grab your pencil or pen and do some drawing. For your convenience, I’ve included links to some of my recommended drawing gear — pencils, pens, sketchbook, and figure mannequin — in this article.
+
+    When you draw, you’re taking time to look at
+    something to analyze it and reproduce it.
+    <h4>8. Be intentional.</h4>
+    This is the hardest thing, because if you want to improve at anything, you must decide to do it. You need to make a commitment and even schedule regular time in your week or your day to pursue drawing. In order to become better at it you need to make a habit of it.
+</div>
 
 </body>
 </html>
--- first/index.html
+++ first/index.html
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>something something</title>
     <link rel="icon" type="image/png" href="https://i.pinimg.com/564x/aa/69/87/aa69876bcaba689b7a7fd96383c7be24.jpg" sizes="16x16">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
 
 </head>
 <body>
